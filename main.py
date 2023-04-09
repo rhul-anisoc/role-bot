937c6e41889fbcc0fb79087e2502122a145a1c89
Leo Durrant <poweredbykurisu@hotmail.com>
2023-04-09 03:44:07 +0100

    sdfsdf

--- main.py
+++ main.py
@@ -5,22 +5,25 @@ import os
 
 class OscarClient(discord.Client):
 
-    def __init__(self):
+    async def on_ready(self):
         self.role_id = os.environ["ROLE_ID"]
 
-    async def on_ready(self):
         print(f"Connected as {self.user}")
         print(f"ROLE_ID = {self.role_id}")
 
-    async def on_member_update(self, before: discord.Member, after: discord.Member):
+    async def on_presence_update(self, before: discord.Member, after: discord.Member):
         try:
             role = discord.utils.get(before.guild.roles, id=self.role_id)
             member = self.get_guild(before.guild.id).get_member(before.id)
+
+            print(
+                f"{member.name} - Pending: (Before: {before.pending}) (After: {after.pending})")
 
             if (before.bot) or (after.bot):
                 return
 
             if before.pending and not after.pending:
+                print(f"Gave role to {member.name}")
                 await member.add_roles(role, atomic=True)
 
         except Exception as e:
@@ -30,7 +33,8 @@ class OscarClient(discord.Client):
 if __name__ == "__main__":
     dotenv.load_dotenv()
 
-    intents = discord.Intents.default()
-    intents.message_content = True
-    client = OscarClient(intents=intents)
+    inte = discord.Intents.default()
+    inte.presences = True
+    inte.members = True
+    client = OscarClient(intents=inte)
     client.run(os.environ['TOKEN'])
